016cba809dbf40763df855082ac0191c5eb44eba
Yatish Agarwal <96855947+thebiggun@users.noreply.github.com>
2024-12-24 19:13:31 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
-  safelist: [
-    'text-gray-800',
-    'text-red-900',
-    'text-blue-900',
-    // Add any other darker shades
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",
+           './public/index.html'],
   theme: {
     extend: {},
   },
